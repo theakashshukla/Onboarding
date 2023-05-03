fbb491d195b9b93572261aa70e5d317b39a49af1
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-05-04 03:22:17 +0530

    Checks

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import AdminPage from "./admin";
 import OnboardPage from "./onboard";
-import OnboardingPage from "./onboarding";
+
 
 
 export default function Home() {
   return (
     <>
       <AdminPage/>
-      <OnboardingPage/>
+      
       <OnboardPage/>
     </>
   )
